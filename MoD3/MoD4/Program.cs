0272bbf9ca6be4316f044de15d01c55867a9bc45
SDenisP <denissokolov1995@mail.ru>
2022-05-07 17:01:36 +0300

    Модуль 4, юнит 1

---
+++ MoD3/MoD4/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+
+		var color = Console.ReadLine();
+
+			if (color == "red")
+			{
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is red!");
+			}
+     
+			else if (color == "cyan")
+			{
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.ForegroundColor = ConsoleColor.Black;
+			}
+
+			else if (color == "green")
+			{
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.ForegroundColor = ConsoleColor.Black;
+
+				Console.WriteLine("Your color is green!");
+			}
+			else
+			{
+				Console.BackgroundColor = ConsoleColor.Yellow;
+				Console.ForegroundColor = ConsoleColor.Red;
+
+				Console.WriteLine("Your color is cyan!");
+			}
+
+			Console.WriteLine();
+			Console.WriteLine("Введите название вашего любимого цвета с маленькой буквы");
+
+			switch(Console.ReadLine())
+			{
+				case "red":
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.ForegroundColor = ConsoleColor.Black;
+				break;
+
+				case "cyan":
+				Console.BackgroundColor = ConsoleColor.Cyan;
+				Console.ForegroundColor = ConsoleColor.Black;
+				break;
+
+				case "green":
+				Console.BackgroundColor = ConsoleColor.Green;
+				Console.ForegroundColor = ConsoleColor.Black;
+				break;
+
+				case "gray":
+				Console.BackgroundColor = ConsoleColor.Gray;
+				Console.ForegroundColor = ConsoleColor.DarkGray;
+				break;
+
+				default:
+				Console.BackgroundColor = ConsoleColor.Yellow;
+				Console.ForegroundColor = ConsoleColor.Red;
+				break;
+
+			}
+		
+	}
+}
