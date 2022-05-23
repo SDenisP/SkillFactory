fe48aea7aa711edd9bfa3b5aa8d958dc9f9f87ee
SDenisP <denissokolov1995@mail.ru>
2022-05-23 22:40:01 +0300

    Модуль 5

--- MoD3/MoD5/Program.cs
+++ MoD3/MoD5/Program.cs
@@ -1,41 +1,39 @@
 ﻿using System;
+using System.Linq;
 
 class MainClass
 {
-    static void ShowArray(int[] massiv, bool logik = false)
+
+    static void ChangeAge(byte Age)     //Метод ввода возраста
+    {
+        Console.WriteLine("Введите возраст: ");
+        byte AgeUser = byte.Parse(Console.ReadLine());
+    }
+    static void GetName(ref string name)    //Метод ввода имени
+    {
+        Console.WriteLine("Введите имя: ");
+        name = Console.ReadLine();
+    }
+    static void ShowArray(int[] massiv, bool logik = false)     //Перебор массива
     {
         var temp = massiv;
         int[] result = new int[] { 12, 34, 45, 1, 6, 7, 23 };
-        if(logik)
+        if (logik)
         {
             temp = SortArray(massiv);
         }
-        foreach(var exit in temp)
+        foreach (var exit in temp)
         {
             Console.WriteLine(exit + " ");
         }
     }
-    static void ShowColor(string username, params string[] favcolors)
-    {
-
-        Console.WriteLine("Ваши любимые цвета:");
-        foreach (var color in favcolors)
-        {
-            Console.WriteLine(color);
-        }
-
-    }
-    static int[] GetArrayFromConsole(int num = 3)
+    static int[] GetArrayFromConsole(ref int num)       // Указываем размерность массива
     {
         var result = new int[num];
-        for (int i = 0; i < result.Length; i++)
-        {
-            Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-            result[i] = int.Parse(Console.ReadLine());
-        }
+        num = 6;
         return result;
     }
-    static int[] SortArray(int[] result)
+    static int[] SortArray(int[] result)        //Сортировка массива
     {
         int number;
         for (int a = 0; a < result.Length; a++)
@@ -52,23 +50,92 @@ class MainClass
         }
         return result;
     }
+    static void BigDataOperation(int[] arr, ref int data)      // Замена элемета массива числом
+    {
+        data = 4;
+        arr[0] = data;
+    }
+    static void Echo(string zvon1, int glub1)       // Рекурсивный метод "Эхо"
+    {
+        var modif = zvon1;
+
+        if (modif.Length > 2)
+        {
+            modif = modif.Remove(0, 2);
+        }
+
+        Console.BackgroundColor = (ConsoleColor)glub1;
+        Console.WriteLine("..." + modif);
+
+        if (glub1 > 1)
+        {
+            Echo(modif, glub1 - 1);
+        }
+    }   
+    static decimal Factorial(decimal x)     //Вычисление факториала
+    {
+        if (x == 0)
+        {
+            return 1;
+        }
+
+        else
+        {
+            return x * Factorial(x - 1);
+        }
+    }
+    private static int PowerUp(int N, byte pow)   // Возведение в степень
+    {
+        if (N == 0 || pow == 0)
+        {
+            return 1;
+        }
+
+        else
+        {
+            if (pow == 1)
+            {
+                return N;
+            }
+            else
+            {
+                return N * PowerUp(N, --pow);
+            }
+        }
+    }  
     public static void Main(string[] args)
     {
 
-        var array = GetArrayFromConsole(10);
-        ShowArray(array, true);
+        Console.WriteLine("Введите число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите степень: ");
+        byte b = byte.Parse(Console.ReadLine());
+        Console.WriteLine(PowerUp(a, b));
 
 
 
 
 
 
+        Console.WriteLine("Введите число для расчёта факториала: ");
+        decimal torial = decimal.Parse(Console.ReadLine());
+        Console.WriteLine(Factorial(torial));
 
 
 
 
 
 
+        Console.WriteLine("Ты меня слышишь?");
+        var zvon = Console.ReadLine();
+
+        Console.WriteLine("Укажите глубину эха");
+        int glub = int.Parse(Console.ReadLine());
+
+        Echo(zvon, glub);
+
+      
         /*
         var favcolors = new string[3];
       
@@ -105,26 +172,11 @@ class MainClass
                     Console.WriteLine("Your color is yellow!");
                     break;
             }
-       // }
-       //
+        }
+       
         ShowColor(favcolors[0], favcolors[2]);
         
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        /////////////////////////////////////////////////////////
         
         var (name, age) = ("Евгения", 27);
         Console.WriteLine("Моё имя {0}", name);
@@ -154,16 +206,7 @@ class MainClass
 
         Console.WriteLine();
 
-
-
-
-
-
-
-
-
-
-
+        //////////////////////////////////////////////////////
 
         string favcolors = new string[3];
 
